5ce66fb589cb58b0e165f7b06a74e8971a6f5cc4
jiazhibin0310 <xxjzb0317@163.com>
2017-04-25 11:12:19 +0800

    ARScan

---
+++ AR_ScanDemo/Assets/AR_Camera_set.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AR_Camera_set : MonoBehaviour {
+
+    // Use this for initialization
+    private string label;
+    private float touchduration;
+    private Touch touch;
+
+    // Use this for initialization  
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame  
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        if (Input.touchCount > 0)
+        {
+            touchduration += Time.deltaTime;
+            touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Ended && touchduration < 0.2f)
+            {
+                StartCoroutine("SingleOrDouble");
+            }
+        }
+        else
+        {
+            touchduration = 0;
+        }
+    }
+
+    IEnumerator SingleOrDouble()
+    {
+        yield return new WaitForSeconds(0.3f);
+        if (touch.tapCount == 1)
+        {
+            Debug.Log("Single");
+            OnSingleTapped();
+        }
+        else if (touch.tapCount == 2)
+        {
+            StopCoroutine("SingleOrDouble");
+            Debug.Log("Double");
+            OnDoubleTapped();
+        }
+    }
+
+    private void OnSingleTapped()
+    {
+        TriggerAutoFocus();
+        label = "Tap the Screen!";
+    }
+
+    private void OnDoubleTapped()
+    {
+        label = "Double Tap the Screen!";
+    }
+
+    public void TriggerAutoFocus()
+    {
+        StartCoroutine(TriggerAutoFocusAndEnableContinuousFocusIfSet());
+    }
+
+    private IEnumerator TriggerAutoFocusAndEnableContinuousFocusIfSet()
+    {
+        Vuforia.CameraDevice.Instance.SetFocusMode(Vuforia.CameraDevice.FocusMode.FOCUS_MODE_TRIGGERAUTO);
+        yield return new WaitForSeconds(1.0f);
+        Vuforia.CameraDevice.Instance.SetFocusMode(Vuforia.CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+    }
+
+}
---
+++ AR_ScanDemo/Assets/MvPlay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MvPlay : MonoBehaviour
+{
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        Debug.Log("Movie Player ");
+        bool bSucc = Handheld.PlayFullScreenMovie("newSun.mp4");
+        Debug.Log("bSucc = " + bSucc);
+    }
+
+    //    Update is called once per frame
+
+    void Update()
+    {
+
+        TextAsset myText = GetComponent<TextAsset>();
+        bool bLeftMousePressed = Input.GetMouseButtonDown(0);
+        if (bLeftMousePressed)
+        {
+            Debug.Log("left mouse pressed. need to call device camera!!");
+        }
+    }
+}
